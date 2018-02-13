0b68d69d3a520fd8ba817a549867633df323755b
pinke <xpinke@qq.com>
2018-02-13 14:57:01 +0800

    add removeClient

--- ios/RCTMqtt/RCTMqtt.m
+++ ios/RCTMqtt/RCTMqtt.m
@@ -85,33 +85,42 @@ RCT_EXPORT_METHOD(createClient:(NSDictionary *) options
     resolve([NSNumber numberWithInt:clientRef]);
     
 }
-RCT_EXPORT_METHOD(connect:(int) clientRef) {
+
+RCT_EXPORT_METHOD(removeClient:(int) clientRef
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject) {
     
-    [[[self clients] objectForKey:[NSNumber numberWithInt:clientRef]] connect];
-    
-}
-
-
-RCT_EXPORT_METHOD(disconnect:(int) clientRef) {
-    [[[self clients] objectForKey:[NSNumber numberWithInt:clientRef]] disconnect];
-}
-
-RCT_EXPORT_METHOD(subscribe:(int) clientRef topic:(NSString *)topic qos:(int)qos) {
-    [[[self clients] objectForKey:[NSNumber numberWithInt:clientRef]] subscribe:topic qos:[NSNumber numberWithInt:qos]];
+    [[self clients] removeObjectForKey:[NSNumber numberWithInt:clientRef]];
+    resolve([NSNumber numberWithInt:clientRef]);
     
 }
 
-RCT_EXPORT_METHOD(unsubscribe:(int) clientRef topic:(NSString *)topic) {
-    [[[self clients] objectForKey:[NSNumber numberWithInt:clientRef]] unsubscribe:topic];
+RCT_EXPORT_METHOD(connect:(nonnull NSNumber *) clientRef) {
+    
+    [[[self clients] objectForKey:clientRef] connect];
     
 }
 
-RCT_EXPORT_METHOD(publish:(int) clientRef topic:(NSString *)topic data:(NSString*)data qos:(int)qos retain:(int)retain) {
-    [[[self clients] objectForKey:[NSNumber numberWithInt:clientRef]] publish:topic
-                                                                         data:[data dataUsingEncoding:NSUTF8StringEncoding]
-                                                                          qos:[NSNumber numberWithInt:qos]
-                                                                       retain:(BOOL)retain];
 
+RCT_EXPORT_METHOD(disconnect:(nonnull NSNumber *) clientRef) {
+    [[[self clients] objectForKey:clientRef] disconnect];
+}
+
+RCT_EXPORT_METHOD(subscribe:(nonnull NSNumber *) clientRef topic:(NSString *)topic qos:(nonnull NSNumber *)qos) {
+    [[[self clients] objectForKey:clientRef] subscribe:topic qos:qos]];
+    
+}
+
+RCT_EXPORT_METHOD(publish:(nonnull NSNumber *) clientRef topic:(NSString *)topic data:(NSString*)data qos:(nonnull NSNumber *)qos retain:(BOOL)retain) {
+    [[[self clients] objectForKey:clientRef] publish:topic
+                                                data:[data dataUsingEncoding:NSUTF8StringEncoding]
+                                                 qos:qos
+                                              retain:retain];
+
+}
+
+RCT_EXPORT_METHOD(unsubscribe:(nonnull NSNumber *) clientRef topic:(NSString *)topic) {
+    [[[self clients] objectForKey:clientRef] unsubscribe:topic];
 }
 
 - (void)dealloc
